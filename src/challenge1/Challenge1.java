d75a24a4fb090651128f67c199621a5983296653
JuanRendon <juanrendon1996@hotmail.com>
2022-11-22 17:57:30 -0500

    Mini-challenge 1 correction according to Juan's feedback

--- src/challenge1/Challenge1.java
+++ src/challenge1/Challenge1.java
@@ -1,39 +1,25 @@
 package challenge1;
 
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Challenge1 {
 
-	public static String methodA() {
-		Scanner sc = new Scanner (System.in);
-		System.out.println("Ingrese 'Tipo A' o 'Tipo B': ");
-		String option = "";
-		option += sc.nextLine();
-		String num = "";
-			
+	public static String methodA(String option) {
 		
+		String num = "";
+		char [] chars = "0123456789".toCharArray();
+		Random random = new Random();
+		StringBuffer buffer = new StringBuffer();
+		
+		for (int i=0;i<8;i++){
+		   buffer.append(chars[random.nextInt(chars.length)]);
+		}
+			
 		if (option.equals("Tipo A")) {
-			char [] chars = "0123456789".toCharArray();
-			Random random = new Random();
-			StringBuffer buffer = new StringBuffer();
-			
-			for (int i=0;i<8;i++){
-			   buffer.append(chars[random.nextInt(chars.length)]);
-			}
-			
 			num = "54" + buffer.toString();
-						
 		} else if (option.equals("Tipo B")) {
-			char [] chars = "0123456789".toCharArray();
-			Random random = new Random();
-			StringBuffer buffer = new StringBuffer();
-			
-			for (int i=0;i<8;i++){
-			   buffer.append(chars[random.nextInt(chars.length)]);
-			}
-			
 			num = "08" + buffer.toString();
 		} else {
 			num = "El parámetro de entrada ingresado es incorrecto, por favor ingrese 'Tipo A' o 'Tipo B'";
@@ -42,11 +28,11 @@ public class Challenge1 {
 		return num;
 	}
 	
-	public static Boolean methodB(String word, String [] list) {
+	public static Boolean methodB(String word, List <String> list) {
 			
 		Boolean check = true;
-		
-		if (Arrays.asList(list).contains(word)) {
+				
+		if (list.contains(word)) {
 			check = false;
 		} else {
 			check = true;
@@ -58,10 +44,15 @@ public class Challenge1 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(methodA());
+		String option = "Tipo A";
+		System.out.println(methodA(option));
 		
-		String word = "Enero";
-		String [] list = {"hola", "mundo", "enero", "2022"};
+		String word = "mundo";
+		List<String> list = new ArrayList<String>();
+		list.add("hola");
+		list.add("mundo");
+		list.add("enero");
+		list.add("2022");
 		System.out.println(methodB(word,list));
 		
 	}
